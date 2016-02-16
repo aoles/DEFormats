5ee07f9ce17960112982303bfd6d2b9acb7045ae
aoles <andrzej.oles@gmail.com>
2016-02-16 17:55:20 +0100

    Add argument 'seed' to mockRnaSeqData

--- R/mockRnaSeqData.R
+++ R/mockRnaSeqData.R
@@ -7,8 +7,9 @@
 #' @param output Output format
 #' @param rep Number of replicates for each condition
 #' @param conditions Condition names
+#' @param seed random number generator seed
 #' @param ... Arguments passed to \code{\link[DESeq2]{makeExampleDESeqDataSet}}
-#' @return Depending on the \code{output} setting a matrix or an \code{\link[GenomicRanges]{RangedSummarizedExperiment-class}} object.
+#' @return Depending on the \code{output} setting a matrix or an \code{\link[SummarizedExperiment]{RangedSummarizedExperiment-class}} object.
 #' @template author
 #' @examples
 #' ## count data
@@ -20,10 +21,11 @@
 #' require("SummarizedExperiment")
 #' identical(mockRnaSeqData, assay(mockRnaSeqDataSE))
 #' @export
-mockRnaSeqData = function(output = c("matrix", "RangedSummarizedExperiment"), rep = 3, conditions = c("Case", "Control"), ...) {
+mockRnaSeqData = function(output = c("matrix", "RangedSummarizedExperiment"), rep = 3, conditions = c("Case", "Control"), seed = 0L, ...) {
   output = match.arg(output)
   
-  set.seed(1)
+  set.seed(seed)
+  
   dds = makeExampleDESeqDataSet(m = 2 * rep, ...)
   colnames(dds) = paste(rep(conditions, each = rep), rep(seq_len(rep), 2))
   
